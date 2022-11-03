b814deda083a33a1656b12d9452370d84b833206
Jav-P <javierandrespp@hotmail.com>
2022-11-03 14:56:27 -0500

    navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './pages/add/add.component';
+import { GeneralComponent } from './pages/general/general.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: GeneralComponent,pathMatch:'full'},
+  {path:'general',component: GeneralComponent},
+  {path:'add',component: AddComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GeneralComponent } from './pages/general/general.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddComponent } from './pages/add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeneralComponent,
+    NavbarComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
